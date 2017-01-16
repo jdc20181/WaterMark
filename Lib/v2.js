1f6b209e4fe74643b3c7386896642ed9e16097ff
jdc20181 <jdc20181@gmail.com>
2017-01-16 12:27:44 -0500

    Update v2.js

--- Lib/v2.js
+++ Lib/v2.js
@@ -1,2 +1,10 @@
 //Starting new Version NOT for production use. 
 
+var div = document.createElement("div");
+var style  = document.createElement("style");
+div.innerHTML =  '<div style="opacity: 0.5;   color: BLACK;position: fixed; top: auto; bottom:50%; left: 80%;"> <a href="https://github.com/jdc20181/SpeedTest">Powered By Example</a></div>';
+document.body.appendChild(div);
+document.querySelector('head').write ("<style> a:link,a:visited{opacity:0.5;color:black;text-decoration:none}a:active,a:hover{text-decoration:none opacity: 0.5;color:black;} </style>";
+//This may not work
+                                      
+                                 

64dc7c3cf62192c2535e3f8038a7bc5f1f807c7c
jdc20181 <jdc20181@gmail.com>
2017-01-16 12:33:06 -0500

    Update v2.js

--- Lib/v2.js
+++ Lib/v2.js
@@ -5,6 +5,7 @@ var style  = document.createElement("style");
 div.innerHTML =  '<div style="opacity: 0.5;   color: BLACK;position: fixed; top: auto; bottom:50%; left: 80%;"> <a href="https://github.com/jdc20181/SpeedTest">Powered By Example</a></div>';
 document.body.appendChild(div);
 document.querySelector('head').write ("<style> a:link,a:visited{opacity:0.5;color:black;text-decoration:none}a:active,a:hover{text-decoration:none opacity: 0.5;color:black;} </style>";
-//This may not work
+//This code is working now but has a few bugs:
+ // The Style won't apply to the "a" so the underline text decoration will not leave.                                    
                                       
                                  
